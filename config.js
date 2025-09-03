ebc5bbc54b3c3c6299308fe49a06e5d5af85d7fc
kailashbiswal1992 <64208998+kailashbiswal1992@users.noreply.github.com>
2025-09-03 16:26:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+// --- Firebase Credentials ---
+const firebaseConfig = {
+  apiKey: "AIzaSyDykGxIq2GUDxFkv28NLmdRmiQzlEjvFuw",
+  authDomain: "rento-56bca.firebaseapp.com",
+  projectId: "rento-56bca",
+  storageBucket: "rento-56bca.appspot.com",
+  messagingSenderId: "641174222477",
+  appId: "1:641174222477:web:0c6c5999787c2ca05d14c4",
+  measurementId: "G-T3CSB1Z271"
+};
+
+// --- Twilio Credentials ---
+const twilioConfig = {
+  accountSid: "AC5e3fc2c9dcc7e9fe498773c636cac085",
+  authToken: "aa3a642c0b2556fa96fd32866bb1118a",
+  fromNumber: "+17853476821",
+  toNumber: "+919742040132"
+};
